82b0c75c06aa9b2cd21ea57a9c93eb6f1a1b3b07
Uthra Krishnan <uthrakartik@Uthras-MacBook-Air.local>
2016-02-15 00:05:45 -0800

    Add nphotos.html, gschool.html, and flush out html, css, and jquery

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(funtion(){
+});
+
+
+$('.fade').mouseenter(function() {
+    $(this).fadeOut(200);
+    $('.text').fadeIn();
+
+});
+
+/*$('img')
+.mouseleave(function(){
+  $(this).animate({opacity:1}, 200);
+  $('.text').fadeOut(); */
+});
+});   
